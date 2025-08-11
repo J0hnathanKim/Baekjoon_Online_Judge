5ce66df0a92bac3499dc1cb309747ec7f0766653
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-08-11 13:02:14 +0900

    Create 1181.c

---
+++ 1181.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+
+int main() {
+	int count = 0;
+	scanf("%d", &count);
+	char** string = malloc(count * sizeof(char*));
+
+}
