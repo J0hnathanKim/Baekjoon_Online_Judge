87dbb3816406424ce68ebd901e9349ab9763c8ec
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-08-19 15:12:03 +0900

    Create 4999.c

---
+++ 4999.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<string.h>
+
+int main(){
+	char a[1000];
+	char b[1000];
+	scanf("%s", &a);
+	scanf("%s", &b);
+	if (strlen(a) >= strlen(b))printf("go");
+	else printf("no");
+}
